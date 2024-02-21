8f2fd55bfd650d0725e9fbebb880187f9a670568
Sampleritgithub32 <103262406+Sampleritgithub32@users.noreply.github.com>
2024-02-21 22:52:06 +0530

    Add files via upload

---
+++ blog-app/src/app/pages/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  featuredPostArray : Array<object>|any;
+  latestPostArray : Array<object>|any;
+  constructor( private postService: PostsService ) {
+
+    
+
+   }
+
+  ngOnInit(): void {
+    this.postService.loadFeatured().subscribe(val =>{
+      this.featuredPostArray = val;
+      
+    });
+
+    this.postService.loadLatest().subscribe(val =>{
+   
+      this.latestPostArray = val;
+
+    })
+  }
+
+}
